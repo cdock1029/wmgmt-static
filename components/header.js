18a85039db8ce4f1a58e9d4e6a0d513854e89e21
Conor Dockry <conordockry@gmail.com>
2019-03-26 13:55:00 -0400

    trying to fix google static maps api error. refactor to now 2 WIP

--- components/header.js
+++ components/header.js
@@ -2,54 +2,33 @@ import React from 'react'
 import Link from 'next/link'
 
 export default class Header extends React.Component {
-
-	// state = {
-	// 	showProperties: false,
-	// }
-	// handleToggleShowProperties = () => {
-	// 	this.setState(prev => ({ showProperties: !prev.showProperties }))
-	// }
 	render() {
-		// const { showProperties } = this.state
 		return (
 			<div>
-				<div className='col'>
-					<div className='bar-wrapper'>
+				<div className="col">
+					<div className="bar-wrapper">
 						<section>
-							<Link prefetch href='/'><a className='brand'>Waldon MGMT</a></Link>
+							<Link prefetch href="/">
+								<a className="brand">Waldon MGMT</a>
+							</Link>
 						</section>
 						<section>
-							<Link prefetch href='/contact'><a>Contact Us</a></Link>
+							<Link prefetch href="/contact">
+								<a>Contact Us</a>
+							</Link>
 						</section>
-						{/* <section className='arrow-wrapper' onClick={this.handleToggleShowProperties}>
-						<span>Properties</span>
-						<div className={`menu-arrow${showProperties ? ' toggled' : ''}`}>
-							<svg style={{ fill: 'inherit' }} width='27px' height='14px' viewBox='0 0 27 14' version='1.1'>
-								<g id='Page-1' stroke='none' strokeWidth='1' fillRule='evenodd'>
-									<polygon id='Arrow' fillRule='nonzero' points='13.4999996 13.9214282 0.6 1.17499997 1.82857139 0.1 13.4999996 11.7714282 25.1714278 0.1 26.3999992 1.17499997'/>
-								</g>
-							</svg>
-						</div>
-					</section> */}
 					</div>
-					<div className='copy slogan'>
-							Come be a good neighbor in a community of good neighbors.
-						</div>
+					<div className="copy slogan">
+						Come be a good neighbor in a community of good neighbors.
+					</div>
 				</div>
-				{/* {showProperties &&
-					<div className='properties-dropdown'>
-						<Link prefetch href='/properties'><a>properties</a></Link>
-						<hr/>
-						<Link prefetch href='/contact'><a>contact</a></Link>
-					</div>
-				} */}
 				<style jsx>{`
 					.bar-wrapper {
 						display: flex;
 						justify-content: space-between;
 						align-items: center;
 						padding: 1em var(--gutter-size);
-						border-bottom: 1px solid rgba(0,0,0,0.5);
+						border-bottom: 1px solid rgba(0, 0, 0, 0.5);
 					}
 					.brand {
 						font-weight: bold;
--- pages/contact.js
+++ pages/contact.js
@@ -1,63 +1,81 @@
 import React from 'react'
 import Page from '../components/Page'
 
+const maybe =
+	'https://maps.googleapis.com/maps/api/staticmap?center%3D41.05%2C-80.813132%26zoom%3D10%26size%3D600x400%26style%3Dfeature%3Aroad.arterial%7Cvisibility%3Aoff%26markers%3D40.8847745%2C-80.7393949%26markers%3D41.1006321%2C-80.7690698%26markers%3D41.1789883%2C-80.7641812%26markers%3D41.1904406%2C-80.9911556&key=AIzaSyCoMLt0HIQyoanE5lEUe2cHIR4Rp9TNlvA'
+const signedUrl =
+	'https://maps.googleapis.com/maps/api/staticmap?center%3D41.05%2C-80.813132%26zoom%3D10%26size%3D600x400%26style%3Dfeature%3Aroad.arterial%7Cvisibility%3Aoff%26markers%3D40.8847745%2C-80.7393949%26markers%3D41.1006321%2C-80.7690698%26markers%3D41.1789883%2C-80.7641812%26markers%3D41.1904406%2C-80.9911556&key=AIzaSyCoMLt0HIQyoanE5lEUe2cHIR4Rp9TNlvA&signature=7PKhRVep_gww3pMc112EJDC2Fnw='
+const MAPS_URL =
+	'maps/api/staticmap?center=41.05,-80.813132&zoom=10&size=600x400&style=feature:road.arterial|visibility:off&markers=40.8847745,-80.7393949&markers=41.1006321,-80.7690698&markers=41.1789883,-80.7641812&markers=41.1904406,-80.9911556&key=AIzaSyCoMLt0HIQyoanE5lEUe2cHIR4Rp9TNlvA'
 export default () => (
-	<Page title='Contact Us to Schedule Apartment Tour'>
-		<div className='contact'>
+	<Page title="Contact Us to Schedule Apartment Tour">
+		<div className="contact">
 			<h3>Contact Us</h3>
 			<table>
 				<tbody>
 					<tr>
-						<td className='address-wrap'>
+						<td className="address-wrap">
 							<address>
-								<span className='name'>Columbiana Manor</span><br/>
-								<span className='street'>337 W Salem St</span><br/>
-								<span className='city-state-zip'>Columbiana, OH 44408</span><br/>
+								<span className="name">Columbiana Manor</span>
+								<br />
+								<span className="street">337 W Salem St</span>
+								<br />
+								<span className="city-state-zip">Columbiana, OH 44408</span>
+								<br />
 							</address>
 						</td>
-						<td className='phone-wrap'>
-							<a href='tel:+13304829323'>(330) 482-9323</a>
+						<td className="phone-wrap">
+							<a href="tel:+13304829323">(330) 482-9323</a>
 						</td>
 					</tr>
 					<tr>
-						<td className='address-wrap'>
+						<td className="address-wrap">
 							<address>
-								<span className='name'>Newton Village</span><br/>
-								<span className='street'>571 Ridge Rd</span><br/>
-								<span className='city-state-zip'>Newton Falls, OH 44444</span><br/>
+								<span className="name">Newton Village</span>
+								<br />
+								<span className="street">571 Ridge Rd</span>
+								<br />
+								<span className="city-state-zip">Newton Falls, OH 44444</span>
+								<br />
 							</address>
 						</td>
-						<td className='phone-wrap'>
-							<a href='tel:+13308727308'>(330) 872-7308</a>
+						<td className="phone-wrap">
+							<a href="tel:+13308727308">(330) 872-7308</a>
 						</td>
 					</tr>
 					<tr>
-						<td className='address-wrap'>
+						<td className="address-wrap">
 							<address>
-								<span className='name'>Niles Executive</span><br/>
-								<span className='street'>925 Youngstown Warren Rd</span><br/>
-								<span className='city-state-zip'>Niles, OH 44446</span><br/>
+								<span className="name">Niles Executive</span>
+								<br />
+								<span className="street">925 Youngstown Warren Rd</span>
+								<br />
+								<span className="city-state-zip">Niles, OH 44446</span>
+								<br />
 							</address>
 						</td>
-						<td className='phone-wrap'>
-							<a href='tel:+13306520609'>(330) 652-0609</a>
+						<td className="phone-wrap">
+							<a href="tel:+13306520609">(330) 652-0609</a>
 						</td>
 					</tr>
 					<tr>
-						<td className='address-wrap'>
+						<td className="address-wrap">
 							<address>
-								<span className='name'>Westchester</span><br/>
-								<span className='street'>4815 Westchester Dr</span><br/>
-								<span className='city-state-zip'>Austintown, OH 44515</span><br/>
+								<span className="name">Westchester</span>
+								<br />
+								<span className="street">4815 Westchester Dr</span>
+								<br />
+								<span className="city-state-zip">Austintown, OH 44515</span>
+								<br />
 							</address>
 						</td>
-						<td className='phone-wrap'>
-							<a href='tel:+13307995758'>(330) 799-5758</a>
+						<td className="phone-wrap">
+							<a href="tel:+13307995758">(330) 799-5758</a>
 						</td>
 					</tr>
 				</tbody>
 			</table>
-			<img src='https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDKoMf5e_-R8DZE1S1RD-tVAHUYt68wJzI&center=41.05%2c%20-80.813132&zoom=10&size=600x400&style=feature:road.arterial|visibility:off&markers=40.8847745,-80.7393949&markers=41.1006321,-80.7690698&markers=41.1789883,-80.7641812&markers=41.1904406,-80.9911556'/>
+			<img src={signedUrl} />
 		</div>
 		<style jsx>{`
 			.contact {
@@ -68,7 +86,9 @@ export default () => (
 				padding: 1em;
 				width: 100%;
 			}
-			img { max-width: 100%; }
+			img {
+				max-width: 100%;
+			}
 			table {
 				width: 100%;
 				max-width: 25em;
@@ -81,11 +101,17 @@ export default () => (
 				white-space: nowrap;
 				text-align: right;
 			}
-			td address { font-style: normal; }
-			td address > .name { font-size: 0.9em; }
-			td address > .street, td address > .city-state-zip {
+			td address {
+				font-style: normal;
+			}
+			td address > .name {
+				font-size: 0.9em;
+			}
+			td address > .street,
+			td address > .city-state-zip {
 				font-size: 0.7em;
-				font-family: -apple-system,BlinkMacSystemFont,Roboto,"Segoe UI","Helvetica Neue",sans-serif;
+				font-family: -apple-system, BlinkMacSystemFont, Roboto, 'Segoe UI',
+					'Helvetica Neue', sans-serif;
 			}
 			td.phone-wrap a {
 				max-width: 100%;
@@ -97,7 +123,6 @@ export default () => (
 				padding: 0.5em 0.7em;
 				font-size: 0.8em;
 			}
-
 		`}</style>
 	</Page>
 )
